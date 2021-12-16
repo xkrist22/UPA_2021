4572525569d0e80f832f67a5f84c0ccaad8a04b5
xkrist22@stud.fit.vutbr.cz <jirka.kristof@seznam.cz>
2021-12-16 15:40:41 +0100

    part of A1 task

--- src/task_A1.py
+++ src/task_A1.py
@@ -6,10 +6,14 @@ __author__ = "NAME NAME"
 __contact__ = "XLOGIN00@stud.fit.vutbr.cz"
 __date__ = "DD-MM-YYYY"
 
+from enum import IntEnum
 from typing import *
 from cassandra.cluster import Cluster
 import logging
 from tqdm import tqdm
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 
 class Task_A1:
@@ -33,6 +37,10 @@ class Task_A1:
         # create cassandra db handler
         self.__cluster = Cluster([ip], port=port)
         self.__session = self.__cluster.connect()
+        self.__session.execute("""
+            USE {};
+        """.format(keyspace))
+
 
 
     def __create_csv(self, filename: str = "A1.csv") -> None:
@@ -44,7 +52,69 @@ class Task_A1:
             filename (str): filename of csv dataset, defaultly "A1.csv"
         """
 
-        #TODO
+        # check if csv file already exists, in this case get data from file
+        try:
+            self.__df = pd.read_csv(filename)
+            return
+        except Exception:
+            pass
+
+        # select dates of infected - for each day there is one date per one infected
+        infected = self.__session.execute("""
+            SELECT date FROM infected;
+        """)
+        # cast date to datetime type, strip days to get only months and aggregate dataframe to format (month, number of infected) 
+        infected = pd.DataFrame(list(infected), columns=["date"])
+        infected["date"] = infected["date"].apply(lambda x: pd.to_datetime(str(x), format="%Y-%m-%d", errors="coerce"))
+        infected = infected.dropna()
+        infected["date"] = infected["date"].dt.strftime("%Y-%m")
+        infected["infected"] = 1
+        infected = infected.groupby(infected["date"]).aggregate({"infected": "sum"})
+
+        # select dates of cured - for each day there is one date per one cured
+        cured = self.__session.execute("""
+            SELECT date FROM cured;
+        """)
+        # cast date to datetime type, strip days to get only months and aggregate dataframe to format (month, number of cured) 
+        cured = pd.DataFrame(list(cured), columns=["date"])
+        cured["date"] = cured["date"].apply(lambda x: pd.to_datetime(str(x), format="%Y-%m-%d", errors="coerce"))
+        cured = cured.dropna()
+        cured["date"] = cured["date"].dt.strftime("%Y-%m")
+        cured["cured"] = 1
+        cured = cured.groupby(cured["date"]).aggregate({"cured": "sum"})
+
+        
+        # select number of people accept for hospitalization per day
+        hospitalized = self.__session.execute("""
+            SELECT date, num_of_first_record FROM hospitalized;
+        """)
+        # cast date to datetime type, strip days to get only months and aggregate dataframe to format (month, number of first records) 
+        hospitalized = pd.DataFrame(list(hospitalized), columns=["date", "hospitalized"])
+        hospitalized["hospitalized"] = hospitalized["hospitalized"].apply(lambda x: pd.to_numeric(int(x), errors="coerce"))
+        hospitalized["date"] = hospitalized["date"].apply(lambda x: pd.to_datetime(str(x), format="%Y-%m-%d", errors="coerce"))
+        hospitalized = hospitalized.dropna()
+        hospitalized["date"] = hospitalized["date"].dt.strftime("%Y-%m")
+        hospitalized = hospitalized.groupby(hospitalized["date"]).aggregate({"hospitalized": "sum"})
+
+        # select number of people accept for tests per day
+        tested = self.__session.execute("""
+            SELECT date, daily_tested_region FROM tested;
+        """)
+        # cast date to datetime type, strip days to get only months and aggregate dataframe to format (month, number of tested) 
+        tested = pd.DataFrame(list(tested), columns=["date", "tested"])
+        tested["tested"] = tested["tested"].apply(lambda x: pd.to_numeric(int(x), errors="coerce"))
+        tested["date"] = tested["date"].apply(lambda x: pd.to_datetime(str(x), format="%Y-%m-%d", errors="coerce"))
+        tested = tested.dropna()
+        tested["date"] = tested["date"].dt.strftime("%Y-%m")
+        tested = tested.groupby(tested["date"]).aggregate({"tested": "sum"})
+
+        # merge data about infected, cured, hospitalized and tested numbers to one dataframe
+        df = pd.merge(infected, cured, on="date", how="outer")
+        df = pd.merge(df, hospitalized, on="date", how="outer")
+        df = pd.merge(df, tested, on="date", how="outer")
+
+        self.__df = df.fillna(0)
+        self.__df.to_csv(filename, sep=",")
 
 
     def plot_graph(self, filename: str = "A1.png") -> None:
@@ -60,4 +130,19 @@ class Task_A1:
         # create dataset first
         self.__create_csv()
 
-        #TODO
+        plt.plot(self.__df.loc[self.__df["algorithm"] == "g", x_axe_var], self.__df.loc[self.__df["algorithm"] == "g", y_axe_var], label="greedy")
+        plt.plot(self.__df.loc[self.__df["algorithm"] == "e", x_axe_var], self.__df.loc[self.__df["algorithm"] == "e", y_axe_var], label="evolution")
+        plt.plot(self.__df.loc[self.__df["algorithm"] == "h", x_axe_var], self.__df.loc[self.__df["algorithm"] == "h", y_axe_var], label="heuristic")
+        plt.legend()
+        plt.xlabel(self.__labels[x_axe_var])
+        plt.ylabel(self.__labels[y_axe_var])
+        if title:
+            plt.title(title)
+        plt.savefig(output_filename)
+        plt.clf()
+
+
+if __name__ == "__main__":
+    # establish connection with default values
+    a1 = Task_A1("localhost", 9042, "covid")
+    a1.plot_graph()
